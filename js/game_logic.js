17808775a7c0806f78cfe635b0320c2e92388ec6
tigerhostsolutions <jgrasty@tigerhost.domains>
2024-12-02 05:17:16 -0500

    Updated to make story images the site background.

--- js/game_logic.js
+++ js/game_logic.js
@@ -15,19 +15,32 @@ function showStoryNode(index) {
   if (story[index]) {
     currentStoryIndex = index;
     const node = story[index];
+
+    // Update the story text
     storyboardEl.innerText = node.text;
 
+    // Update option buttons
     updateButton(optionAEl, node.option[0]);
     updateButton(optionBEl, node.option[1]);
 
-    // Set the background image of the story window
-    if (node.image) {
-      document.querySelector('.story-window').style.backgroundImage = `url(${node.image})`;
-    } else {
-      document.querySelector('.story-window').style.backgroundImage = 'none';
-    }
+    // Update the background of the entire page
+    updatePageBackground(node.image);
+
   } else {
     console.error("Invalid story index:", index);
+  }
+}
+
+function updatePageBackground(image) {
+  const bodyElement = document.body;
+
+  if (image) {
+    bodyElement.style.backgroundImage = `url(${image})`;
+    bodyElement.style.backgroundSize = 'cover';
+    bodyElement.style.backgroundPosition = 'center';
+    bodyElement.style.transition = 'background-image 1s ease-in-out';
+  } else {
+    bodyElement.style.backgroundImage = 'none';
   }
 }
 
